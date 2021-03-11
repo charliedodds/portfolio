76f3f5dddf38e3623ef2b91009442cd4db730e6d
charliedodds <charlesgdodds@hotmail.co.uk>
2021-03-11 16:56:09 +0000

    Refactor index.js

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,14 @@ const navBtn = document.querySelector('.nav-btn');
 const nav = document.querySelector('nav');
 const navLinks = document.querySelectorAll('.nav a');
 
-const handleNavClick = (e) => {
+const toggleClasses = () => {
 	nav.classList.toggle('expand');
 	nav.classList.toggle('collapse');
 	navBtn.classList.toggle('nav-btn--clicked');
+};
+
+const handleNavClick = (e) => {
+	toggleClasses();
 };
 
 for (let link of navLinks) {
